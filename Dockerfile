80b112ba44f762a5e5086c272bb69dcfcbe72e3f
Vovas <vovaafon25@gmail.com>
2025-09-12 00:33:44 +0300

    Solution

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.13-alpine
+
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY app/ .
+
+CMD ["python", "main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,61 @@
+import os
+import sys
+from typing import Any
+
+import requests
+from requests import RequestException
+from dotenv import load_dotenv
+
+
+def validate(smth: Any) -> None:
+    if smth is None:
+        print(f"Error occurred")
+        sys.exit(1)
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    load_dotenv()
+
+    api_key = os.environ.get("API_KEY")
+    q_param = os.environ.get("Q_PARAM")
+
+    if not api_key:
+        print("API key not found. Please set _API_KEY in .env")
+        sys.exit(1)
+    if not q_param:
+        print("Q parameter not found. Please set Q_PARAM in .env")
+        sys.exit(1)
+
+    url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={q_param}"
+
+    try:
+        r = requests.get(url)
+        data = r.json()
+    except RequestException as e:
+        print(f"Error occurred: {e}")
+        sys.exit(1)
+
+    location = data.get("location")
+    validate(location)
+    current = data.get("current")
+    validate(current)
+
+    city = location.get("name")
+    validate(city)
+
+    country = location.get("country")
+    validate(country)
+
+    time = location.get("localtime")
+    validate(time)
+
+    temperature = current.get("temp_c")
+    validate(temperature)
+
+    condition = current.get("condition").get("text")
+    validate(condition)
+
+    print(f"{country}/{city} {time} Weather: {temperature} {condition}")
 
 
 if __name__ == "__main__":
